7c7ff4b2ddc90987bbd88f9c834f82ffbe8046f9
Growth Team <growth@team.dev>
2026-06-13 21:13:23 +0800

    Add Railway deployment config + PORT env support

--- server.py
+++ server.py
@@ -193,4 +193,5 @@ app.mount("/", StaticFiles(directory=str(STATIC_DIR), html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
